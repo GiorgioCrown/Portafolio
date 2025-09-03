a27c834823ec66eddbbd9498303e1374f830e1f5
Jorge <jorge.l.c.940917@gmail.com>
2025-09-03 07:29:06 -0600

    Se cambia el placeholder del input de PokedexControl

--- src/components/Pokedex/PokedexControls.jsx
+++ src/components/Pokedex/PokedexControls.jsx
@@ -27,7 +27,7 @@ const PokedexControls = ({
               type="text"
               value={searchTerm}
               onChange={onSearchChange}
-              placeholder="Buscar Pokémon por nombre..."
+              placeholder="Buscar Pokémon..."
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900"
             />
             {/* Botón de búsqueda posicionado absolutamente */}
